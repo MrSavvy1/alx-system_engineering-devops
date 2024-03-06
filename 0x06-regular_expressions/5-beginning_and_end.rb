04c157e5ca61f09f0558fd8a2947a4763751402c
MrSavvy1 <destydesty112@gmail.com>
2024-03-06 13:31:40 -0800

    innit

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regs that matches a string with h as first letter anf n as last letter  with any letter inbtw
+#regs that matches a string with h as first letter and n as last letter  with any letter inbtw
 puts ARGV[0].scan(/h.n/).join
